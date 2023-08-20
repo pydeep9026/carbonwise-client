ceaec697958d14807e83e3e96409e7c9dd85424a
pradeep singh <pradeepsi2120032@gmail.com>
2023-08-20 10:14:11 +0530

    routes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
 import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './components/store/home/home';
+
+
+
+
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <>
+    <Routes>
+      <Route path='/' element={<Home/>} />
+    </Routes>
+    </>
   );
 }
 export default App;
 
 
 
-/*homepage
-kart
-*/
